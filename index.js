c78047173c7482c64133e5809a6521ef0ef95838
Web <syed.yousuf@navana.com>
2023-02-22 18:23:48 +0600

    Test email code setup

--- index.js
+++ index.js
@@ -4,12 +4,40 @@ require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
+const emailService = process.env.EMAIL_SERVICE;
+const userEmail = process.env.USER;
+const userPass = process.env.PASS;
 
 app.use(express.json());
 
-app.get("/email/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+const transporter = nodemailer.createTransport({
+  service: emailService,
+  auth: {
+    user: userEmail,
+    pass: userPass,
+  },
+});
+
+app.get("/subscribe", async (req, res) => {
+  var mailOptions = {
+    from: userPass,
+    to: userPass,
+    subject: "Test email",
+    text: `Testing email service.`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Email sent succesfully!");
+    res.send("Subscribed!");
+  } catch (err) {
+    console.log("Error while sending email: ", err);
+  }
 });
 
 app.listen(port, () => {
